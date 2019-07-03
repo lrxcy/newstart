d3952aab2d43553fa8ed9dbaa3cefba356fdf363
lrxcy <34975746+lrxcy@users.noreply.github.com>
2019-07-03 20:34:05 +0700

    Update main.go
    test

--- main.go
+++ main.go
@@ -1,30 +1,78 @@
 package main
 
 import (
-    "log"
+	"log"
+	"time"
 
-    sdk "github.com/gaia-pipeline/gosdk"
+	sdk "github.com/gaia-pipeline/gosdk"
 )
 
-// This is one job. Add more if you want.
-func DoSomethingAwesome(args sdk.Arguments) error {
-    log.Println("This output will be streamed back to gaia and will be displayed in the pipeline logs.")
+func CreateUser(args sdk.Arguments) error {
+	log.Println("CreateUser has been started!")
 
-    // An error occured? Return it back so gaia knows that this job failed.
-    return nil
+	// lets sleep to simulate that we do something
+	time.Sleep(5 * time.Second)
+	log.Println("CreateUser has been finished!")
+	return nil
+}
+
+func MigrateDB(args sdk.Arguments) error {
+	log.Println("MigrateDB has been started!")
+
+	// lets sleep to simulate that we do something
+	time.Sleep(5 * time.Second)
+	log.Println("MigrateDB has been finished!")
+	return nil
+}
+
+func CreateNamespace(args sdk.Arguments) error {
+	log.Println("CreateNamespace has been started!")
+
+	// lets sleep to simulate that we do something
+	time.Sleep(5 * time.Second)
+	log.Println("CreateNamespace has been finished!")
+	return nil
+}
+
+func CreateDeployment(args sdk.Arguments) error {
+	log.Println("CreateDeployment has been started!")
+
+	// lets sleep to simulate that we do something
+	time.Sleep(5 * time.Second)
+	log.Println("CreateDeployment has been finished!")
+	return nil
+}
+
+func CreateService(args sdk.Arguments) error {
+	log.Println("CreateService has been started!")
+
+	// lets sleep to simulate that we do something
+	time.Sleep(5 * time.Second)
+	log.Println("CreateService has been finished!")
+	return nil
+}
+
+func CreateIngress(args sdk.Arguments) error {
+	log.Println("CreateIngress has been started!")
+
+	// lets sleep to simulate that we do something
+	time.Sleep(5 * time.Second)
+	log.Println("CreateIngress has been finished!")
+	return nil
+}
+
+func Cleanup(args sdk.Arguments) error {
+	log.Println("Cleanup has been started!")
+
+	// lets sleep to simulate that we do something
+	time.Sleep(5 * time.Second)
+	log.Println("Cleanup has been finished!")
+	return nil
 }
 
 func main() {
-    jobs := sdk.Jobs{
-        sdk.Job{
-            Handler:     DoSomethingAwesome,
-            Title:       "DoSomethingAwesome",
-            Description: "This job does something awesome.",
-        },
-    }
-
-    // Serve
-    if err := sdk.Serve(jobs); err != nil {
-        panic(err)
-    }
+	// Serve
+	if err := sdk.Serve(jobs); err != nil {
+		panic(err)
+	}
 }
